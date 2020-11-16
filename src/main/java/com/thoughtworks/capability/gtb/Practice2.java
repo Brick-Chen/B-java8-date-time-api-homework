a5dde770fc8b05f433d04a092b98d5ffeb4de5c8
YumingChen <mathcym@outlook.com>
2020-11-16 21:46:29 +0800

    [yuming chen]: finish practice 2.

--- src/main/java/com/thoughtworks/capability/gtb/Practice2.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice2.java
@@ -9,8 +9,18 @@ import java.time.LocalDate;
  * @create 2020-05-15_17:20
  */
 public class Practice2 {
-
+  private final static int FRIDAY = 5;
+  private final static int SUNDAY = 7;
   public static LocalDate getNextWorkDate(LocalDate date) {
-    return null;
+    int dayOfWeek = date.getDayOfWeek().getValue();
+    int dayToNextWorkDay = 0;
+    if (dayOfWeek < FRIDAY || dayOfWeek == SUNDAY) {
+      dayToNextWorkDay += 1;
+    } else if (dayOfWeek == FRIDAY) {
+      dayToNextWorkDay += 3;
+    } else {
+      dayToNextWorkDay += 2;
+    }
+    return date.plusDays(dayToNextWorkDay);
   }
 }
